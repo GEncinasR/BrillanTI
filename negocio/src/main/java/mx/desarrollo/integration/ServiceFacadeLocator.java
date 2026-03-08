66e88c80b8cc34586c4e963b97d2d039c9665034
Mariel <mariel.torres@uabc.edu.mx>
2026-03-08 16:12:54 -0700

    capa negocio terminada

--- negocio/src/main/java/mx/desarrollo/integration/ServiceFacadeLocator.java
+++ negocio/src/main/java/mx/desarrollo/integration/ServiceFacadeLocator.java
@@ -1,12 +1,24 @@
 package mx.desarrollo.integration;
 
 import mx.desarrollo.facade.FacadeAlumno;
+import mx.desarrollo.facade.FacadeProfesor;
 import mx.desarrollo.facade.FacadeUsuario;
 
 public class ServiceFacadeLocator {
 
     private static FacadeAlumno facadeAlumno;
     private static FacadeUsuario facadeUsuario;
+    private static FacadeProfesor facadeProfesor;
+
+    private static FacadeProfesor getInstanceFacadeProfesor(){
+        if(facadeProfesor == null){
+            facadeProfesor = new FacadeProfesor();
+            return facadeProfesor;
+        } else {
+            return facadeProfesor;
+        }
+    }
+
 
     public static FacadeAlumno getInstanceFacadeAlumno() {
         if (facadeAlumno == null) {
